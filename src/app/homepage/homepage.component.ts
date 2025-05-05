539f70032cfbbb20d7cb0a9285526e2403a9bc26
Angela Slinker <angela@Angelas-MacBook-Pro.local>
2025-05-05 09:56:11 -0700

    Fixed Modals, updated backdrop and colors

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -18,40 +18,44 @@ interface Folder {
   styleUrl: './homepage.component.css'
 })
 export class HomepageComponent {
-
+  selectedFolderId: string | null = null;
   folders: Folder[] = [
     {
       id: 'resume',
       label: 'Resume',
-      imgSrc: '/folderIcon.png',
+      imgSrc: '/beigeFinder.png',
       top: '20px',
       left: '40px'
     },
     {
       id: 'projects',
       label: 'Projects',
-      imgSrc: '/folderIcon.png',
+      imgSrc: '/beigeFinder.png',
       top: '20px',
       left: '160px'
     },
     {
       id: 'projects',
       label: 'Projects',
-      imgSrc: '/folderIcon.png',
+      imgSrc: '/beigeFinder.png',
       top: '20px',
       left: '1080px'
     },
     {
       id: 'projects',
       label: 'Projects',
-      imgSrc: '/folderIcon.png',
+      imgSrc: '/beigeFinder.png',
       top: '20px',
       left: '960px'
     }
   ];
 
   openFolder(id: string) {
-    console.log('clicked', id);
+    this.selectedFolderId = id;
+  }
+
+  closeModal() {
+    this.selectedFolderId = null;
   }
 
 }
